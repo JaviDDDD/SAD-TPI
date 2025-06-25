0c076c1ae37f381a869fbc6cbce4245ec697d4b6
Javier Dojas <dojas.javier@gmail.com>
2025-06-25 20:15:39 -0300

    ABM tareas

--- srv/src/controllers/TareaController.ts
+++ srv/src/controllers/TareaController.ts
@@ -1,17 +1,100 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import { TareaService } from "../services/TareaService";
+import { Tarea } from "../entity/Tarea";
 
 export class TareaController {
   router: Router;
   path: string;
+  tareaService: TareaService;
 
   constructor() {
     this.path = "/tarea";
     this.router = Router();
-    this.router.get("/", this.handleGetHome.bind(this));
+    this.tareaService = new TareaService();
+    this.router.get("/", this.handleGet.bind(this));
+    this.router.post("/", this.handlePost.bind(this));
+    this.router.delete("/:id", this.handleDelete.bind(this));
+    this.router.patch("/:id", this.handlePatch.bind(this));
   }
 
-  private async handleGetHome(req: Request, res: Response) {
-    res.json({ message: "test" });
+  private async handleGet(req: Request, res: Response) {
+    try {
+      const tareas = await this.tareaService.getTareas();
+      res.status(200).json(tareas);
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+    }
+  }
+
+  private async handlePost(req: Request, res: Response) {
+    try {
+      const nombre = req.body.nombre;
+      if (!nombre) {
+        res.status(400).send({ message: "El nombre es obligatorio" });
+        return;
+      }
+
+      const tarea = await this.tareaService.createTarea(nombre);
+
+      res.status(201).json(tarea);
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+    }
+  }
+
+  private async handleDelete(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        res.status(400).send({ message: "La id es obligatoria" });
+        return;
+      }
+
+      const tarea = await this.tareaService.getById(id);
+      if (!tarea) {
+        res.status(404).send({ message: "Tarea no encontrada" });
+        return;
+      }
+
+      const deletedResult = await this.tareaService.deleteById(tarea.id);
+
+      if (
+        deletedResult &&
+        deletedResult.affected &&
+        deletedResult.affected > 0
+      ) {
+        res.status(200).json({ deleted: true });
+      } else {
+        res.status(204).json({ deleted: false });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+    }
+  }
+
+  private async handlePatch(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      if (!id) {
+        res.status(400).send({ message: "La id es obligatoria" });
+        return;
+      }
+
+      let tarea = await this.tareaService.getById(id);
+      if (!tarea) {
+        res.status(404).send({ message: "Tarea no encontrada" });
+        return;
+      }
+
+      tarea = await this.tareaService.toggleCompletada(tarea);
+      res.status(200).json(tarea);
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+    }
   }
 }
