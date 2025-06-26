19d3020b6e40db448c5885815a1c6a73a5b46a7c
unknown <pab.a.bari@gmail.com>
2025-06-26 18:08:17 -0300

    Inicio Front

--- ng/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/v1',
-  chatPollingInterval: 1000
-};
--- ng/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '/v1',
-  chatPollingInterval: 5000
-};
