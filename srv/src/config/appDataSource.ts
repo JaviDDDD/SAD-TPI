bb194bf6e8fb2f9fe0cce62b49a11ad6ab3cce02
Ignacio Fiora <ignaciofiora@gmail.com>
2025-06-28 15:17:03 -0300

    cambios backend

--- srv/src/config/appDataSource.ts
+++ srv/src/config/appDataSource.ts
@@ -2,13 +2,16 @@ import { Tarea } from '../entity/Tarea';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [Tarea],
-  synchronize: true, // set to false in production
+  synchronize: true,
   logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
